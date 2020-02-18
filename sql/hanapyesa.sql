6506bb9a605c6fef799428761d6e2ecbc70628ba
ianrey258 <ianreyinfinity@gmail.com>
2020-02-18 21:04:49 +0800

    Map added and etc..

--- sql/hanapyesa.sql
+++ sql/hanapyesa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 16, 2020 at 06:50 PM
+-- Generation Time: Feb 18, 2020 at 02:03 PM
 -- Server version: 10.1.40-MariaDB
 -- PHP Version: 7.1.29
 
